c888d9f34614a2ad28865a2c78be2a971481a1e4
luanrafael <luan-g8@hotmail.com>
2013-07-26 22:58:40 -0300

    Initial commit

---
+++ src/main/java/com/android/love/AndroidLove.java
@@ -0,0 +1,20 @@
+package com.android.love;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by luan on 24/07/13.
+ */
+
+public class AndroidLove extends Activity {
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
